536e042dcc7e94e7bea6afd02cc03d5fd2d79995
Matias Scruz <144630285+matscruz@users.noreply.github.com>
2025-09-02 00:29:28 +0000

    criar classe tv

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+
 Console.WriteLine("Hello, World!");
+
+Televisao tv = new Televisao(55f);
+
+tv.Tamanho = 22.5f;
+
+Console.WriteLine($"A tv tem o tamanho {tv.Tamanho}");
+
+tv.Volume = -35;
+Console.WriteLine($"volume {tv.Volume}");
+
+tv.Volume = 120;
+Console.WriteLine($"volume {tv.Volume}");
